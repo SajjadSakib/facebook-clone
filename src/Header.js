8198bf35fac7c0ae366db9d83d59d921304a624a
SajjadSakib <sajjadsakib76@gmail.com>
2021-05-03 22:26:34 +0600

    done with header

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,11 @@ import GroupIcon from '@material-ui/icons/Group';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import StorefrontIcon from '@material-ui/icons/Storefront';
 import FlagIcon from '@material-ui/icons/Flag';
+import SearchIcon from '@material-ui/icons/Search';
+import { IconButton,Avatar } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 
 function Header(){
@@ -13,10 +18,11 @@ function Header(){
         <div>
           <img
               src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1024px-Facebook_f_logo_%282019%29.svg.png'
-              height='50px'/>
+              height='50px' alt='facebook'/>
         </div>
         <div className='SearchField'>
-          <input className='Search' placeholder='Search'/>
+          <SearchIcon />
+          <input className='Search' placeholder='Search in Facebook'/>
         </div>
       </div>
       <div className='header_middle'>
@@ -37,10 +43,33 @@ function Header(){
         </div>
         
       </div>
+      
       <div className='header_right'>
-      left
+      
+      <div className='header_button'>
+      <IconButton>
+       <Avatar />
+      </IconButton>
+      </div>
+       <div className='header_button'>
+      <IconButton>
+       <AddIcon />
+      </IconButton>
+      </div>
+      <div className='header_button'>
+      <IconButton>
+       
+       <ForumIcon />
+      </IconButton>
+      </div>
+      <div className='header_button'>
+      <IconButton>
+       <SettingsIcon />
+       
+      </IconButton>
       </div>
       
+    </div>
     </div>
     
    )
