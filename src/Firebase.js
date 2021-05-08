fffc70ff42491f3d9eece37b4ba3fd6c6bd17067
SajjadSakib <67702070+SajjadSakib@users.noreply.github.com>
2021-05-08 18:22:43 +0600

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBC-lhz3DH1PlrEbjkgJv90pUif0nssESc",
-    authDomain: "faccebook-a8bb0.firebaseapp.com",
-    projectId: "faccebook-a8bb0",
-    storageBucket: "faccebook-a8bb0.appspot.com",
-    messagingSenderId: "332354057127",
-    appId: "1:332354057127:web:db59b7027965af826d7c82",
-    measurementId: "G-KCP7KW1QXQ"
+    
   };
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
